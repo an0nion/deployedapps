14d4b607e8e8b38b05510e38b6659a38950709b7
onion <valkyriecain.darquessevile@gmail.com>
2024-06-24 05:13:00 +1000

    Update 4_Geopolitical Semiconductor Legislation.py

--- pages/4_Geopolitical Semiconductor Legislation.py
+++ pages/4_Geopolitical Semiconductor Legislation.py
@@ -1,3 +1,36 @@
 import streamlit as st
 
-st.title("Geopolitical legislation in the semiconductor industry")
+st.title("Geopolitical Legislation in the Semiconductor Industry")
+
+st.caption("_by :blue[Ananya Salian]_")
+st.caption("Currently in progress!")
+st.caption("Expected to be up & running: **_10th July, 2024_**")
+
+st.caption("Most legislation implemented aims to reduce supply chain disruptions\
+           ,reduce dependence on foreign suppliers, and to increase\
+           a regions share of global semiconductor production")
+
+st.write("_**US:**_")
+st.write("""
+         _CHIPS Act_:Allocating $280B to the US for R&D and manufacturing support to reduce reliance on foreign manufacturers.
+        - Chip EQUIP Act (Not Yet Passed): Seeks to ban semiconductor manufacturers funded under the CHIPS Act from using specialized manufacturing equipment sourced from a 'foreign entity of concern' to 'stop China and other foreign entities of concern from compromising our microchip manufacturing facilities,' and strengthen national security.
+        - Increased profitability for major beneficiaries of the subsidies and tax incentives (likely US firms like AMD and Intel).
+        - Reduced dependence on foreign suppliers.
+        - Likely to increase trade tensions with China due to limiting their profitability as a result of this Act.
+        \n\n
+         """)
+st.write("_**China:**_")
+st.write("""
+        _China Integrated Circuit Industry Investment Fund (ICF)_: Consisting of 3 phases, raising over US$77B over the course of 2014-2039 to reduce dependence on external manufacturers and achieve self-sufficiency.
+        - Chinese firms such as SMIC, Hua Hong Semiconductor, and YMTC will see significant profits due to increased government investments. However, for firms that operate/trade outside of China, as well as foreign firms that trade with China, there may be supply or operational issues.    
+          \n  \n 
+         """)
+
+st.write("_**EU:**_")
+st.write("""
+         _European Chips Act_: Investment of $47B USD to double the EU's global semiconductor production to 20% by 2030, aiming to enhance R&D and reduce the risk of supply chain disruptions.
+         - Domestic semiconductor companies such as ASML and Infineon will likely see increased profits due to the provision of funding and subsidies as well as higher demand for domestic semiconductors. 
+         \n\n
+         """)
+
+
