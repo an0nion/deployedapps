7ff160bbec6ca61fd4f42f50d5a0c1c491c67342
onion <valkyriecain.darquessevile@gmail.com>
2024-09-12 17:29:11 +1000

    Update 4_🌏_Geopolitical Semiconductor Legislation.py

--- semiconductors/pages/4_🌏_Geopolitical Semiconductor Legislation.py
+++ semiconductors/pages/4_🌏_Geopolitical Semiconductor Legislation.py
@@ -11,7 +11,7 @@ st.caption("Most legislation implemented aims to reduce supply chain disruptions
            a regions share of global semiconductor production")
 st.write("_**Taiwan**_")
 st.write("""
-           _Industrial Innovation Act_:Chipmakers will be able to file for tax credits of 5% of their annual costs for purchasing equipment for advanced nodes (usually the costliest part of chip manufacturing) and a 25% tax bread on R&D expenses
+           _Industrial Innovation Act_:  Chipmakers will be able to file for tax credits of 5% of their annual costs for purchasing equipment for advanced nodes (usually the costliest part of chip manufacturing) and a 25% tax bread on R&D expenses
             - Taiwanese Ministery of Economic Affairs states that this will encourage higher domestic investment in homegrown tech businessses like TSMC, MediaTek, GlobalWafers, UMC, Phison,  especially for R&D
             - Aims to maintain Taiwan's competitive edge in the semiconductor industry, currently holding 68% of the global semiconductor market, with TSMC alone with a market share of 53%.
             \n\n
@@ -20,7 +20,7 @@ st.write("""
 
 st.write("_**US:**_")
 st.write("""
-         _CHIPS Act_:Allocating $280B to the US for R&D and manufacturing support to reduce reliance on foreign manufacturers.
+         _CHIPS Act_:  Allocating $280B to the US for R&D and manufacturing support to reduce reliance on foreign manufacturers.
         - Chip EQUIP Act (Not Yet Passed): Seeks to ban semiconductor manufacturers funded under the CHIPS Act from using specialized manufacturing equipment sourced from a 'foreign entity of concern' to 'stop China and other foreign entities of concern from compromising our microchip manufacturing facilities,' and strengthen national security.
         - Increased profitability for major beneficiaries of the subsidies and tax incentives (likely US firms like AMD and Intel).
         - Reduced dependence on foreign suppliers.
@@ -29,14 +29,14 @@ st.write("""
          """)
 st.write("_**China:**_")
 st.write("""
-        _China Integrated Circuit Industry Investment Fund (ICF)_: Consisting of 3 phases, raising over US$77B over the course of 2014-2039 to reduce dependence on external manufacturers and achieve self-sufficiency.
+        _China Integrated Circuit Industry Investment Fund (ICF)_:  Consisting of 3 phases, raising over US$77B over the course of 2014-2039 to reduce dependence on external manufacturers and achieve self-sufficiency.
         - Chinese firms such as SMIC, Hua Hong Semiconductor, and YMTC will see significant profits due to increased government investments. However, for foreign firms that operate or trade with China, there may be operation or supply chain issues as a result of China's increased focus on domestic production and reducing dependence on foreign partners.    
           \n  \n 
          """)
 
 st.write("_**EU:**_")
 st.write("""
-         _European Chips Act_: Investment of $47B USD to double the EU's global semiconductor production to 20% by 2030, aiming to enhance R&D and reduce the risk of supply chain disruptions.
+         _European Chips Act_:  Investment of $47B USD to double the EU's global semiconductor production to 20% by 2030, aiming to enhance R&D and reduce the risk of supply chain disruptions.
          - Domestic semiconductor companies such as ASML and Infineon will likely see increased profits due to the provision of funding and subsidies as well as higher demand for domestic semiconductors. 
          \n\n
          """)
